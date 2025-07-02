82c9e378b8a96097e66985bc55c0ada3732394ce
Ricardo Persaud <56775968+ricardo39985@users.noreply.github.com>
2025-07-02 03:59:13 -0400

    Polish login and dashboard pages

--- app/javascript/pages/Auth/Login.vue
+++ app/javascript/pages/Auth/Login.vue
@@ -1,12 +1,26 @@
 <template>
   <Head title="Login" />
-  <v-container class="fill-height" justify="center" align="center">
-    <v-card width="420" elevation="2">
-      <v-card-title class="justify-center">Sign in to FixFlow</v-card-title>
+  <v-container class="fill-height d-flex align-center justify-center px-4">
+    <v-card max-width="420" class="w-100" elevation="2">
+      <v-card-title class="justify-center text-h6">Sign in to FixFlow</v-card-title>
       <v-card-text>
-        <v-form @submit.prevent="submit" data-turbo="false">
-          <v-text-field v-model="form.user.email" label="Email" type="email" outlined required class="mb-3" />
-          <v-text-field v-model="form.user.password" label="Password" type="password" outlined required class="mb-4" />
+        <v-form @submit.prevent="submit" data-turbo="false" class="d-flex flex-column">
+          <v-text-field
+            v-model="form.user.email"
+            label="Email"
+            type="email"
+            outlined
+            required
+            class="mb-3"
+          />
+          <v-text-field
+            v-model="form.user.password"
+            label="Password"
+            type="password"
+            outlined
+            required
+            class="mb-4"
+          />
           <v-btn type="submit" :loading="form.processing" color="primary" block>Login</v-btn>
         </v-form>
       </v-card-text>
--- app/javascript/pages/Dashboard.vue
+++ app/javascript/pages/Dashboard.vue
@@ -2,13 +2,41 @@
   <Head title="Dashboard" />
   <v-container class="pa-4">
     <h1 class="text-h4 mb-4">Welcome</h1>
-    <p class="text-body-1">You are logged in as {{ user.email }}.</p>
+    <p class="text-body-1 mb-6">You are logged in as {{ user.email }}.</p>
+
+    <v-card elevation="1" class="mb-4">
+      <v-card-title class="text-h6">Tech Stack</v-card-title>
+      <v-list>
+        <v-list-item
+          v-for="item in stack"
+          :key="item.name"
+          :href="item.url"
+          target="_blank"
+          class="hover:bg-grey-lighten-4"
+        >
+          <v-list-item-title>{{ item.name }}</v-list-item-title>
+          <template #append>
+            <v-icon size="small">mdi-open-in-new</v-icon>
+          </template>
+        </v-list-item>
+      </v-list>
+    </v-card>
   </v-container>
 </template>
 
 <script setup>
 import { Head } from '@inertiajs/vue3'
+
 defineProps({
   user: Object
 })
+
+const stack = [
+  { name: 'Ruby on Rails', url: 'https://rubyonrails.org' },
+  { name: 'PostgreSQL', url: 'https://www.postgresql.org/docs/' },
+  { name: 'Vue.js', url: 'https://vuejs.org' },
+  { name: 'Vuetify', url: 'https://vuetifyjs.com' },
+  { name: 'Inertia.js', url: 'https://inertiajs.com' },
+  { name: 'Vite', url: 'https://vitejs.dev' },
+]
 </script>

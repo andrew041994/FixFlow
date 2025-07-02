d03e4d98cdf67aecb9acec466574b32317814dd4
Ricardo Persaud <56775968+ricardo39985@users.noreply.github.com>
2025-07-02 03:39:08 -0400

    Setup Devise auth with Inertia

---
+++ app/javascript/pages/Auth/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <Head title="Login" />
+  <v-container class="fill-height" justify="center" align="center">
+    <v-card width="400">
+      <v-card-title>Login</v-card-title>
+      <v-card-text>
+        <v-form @submit.prevent="submit">
+          <v-text-field v-model="form.email" label="Email" type="email" required></v-text-field>
+          <v-text-field v-model="form.password" label="Password" type="password" required></v-text-field>
+          <v-btn type="submit" :loading="form.processing" color="primary" block>Login</v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script setup>
+import { Head, useForm } from '@inertiajs/vue3'
+
+const form = useForm({
+  email: '',
+  password: ''
+})
+
+function submit() {
+  form.post('/users/sign_in')
+}
+</script>
---
+++ app/javascript/pages/Dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+  <Head title="Dashboard" />
+  <v-container class="pa-4">
+    <h1>Dashboard</h1>
+    <p>You are logged in!</p>
+  </v-container>
+</template>
+
+<script setup>
+import { Head } from '@inertiajs/vue3'
+</script>
